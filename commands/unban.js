9bca7e6c67debfff256b51ebaf1cc6e522737451
Edgars Eduards Hoveckis <53938497+EEHoveckis@users.noreply.github.com>
2020-05-30 01:51:02 +0300

    testing

--- commands/unban.js
+++ commands/unban.js
@@ -12,11 +12,11 @@ module.exports = {
 	disabled: false,
 	execute(client, message, args) {
 		const userID = args[0];
-		const user = client.users.resolveID(userID);
+		const user = client.users.fetch(userID);
 		if(!user) return message.channel.send(`Couldn't find user to unban!\nPlease try again, ${message.author}!`);
 		const reason = args.slice(1).join(" ");
 
-		message.guild.members.unban(user)
-			.then(user => message.channel.send(`Unbanned ${user.username}!`));
+		//message.guild.members.unban(user)
+			//.then(user => message.channel.send(`Unbanned ${user.username}!`));
 	},
 };
