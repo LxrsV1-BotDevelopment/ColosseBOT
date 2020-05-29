d3d139e4769b187d4931c698a3dbaa246b0181df
Edgars Eduards Hoveckis <53938497+EEHoveckis@users.noreply.github.com>
2020-05-30 01:45:28 +0300

    Fixed typo

--- commands/unban.js
+++ commands/unban.js
@@ -12,8 +12,8 @@ module.exports = {
 	disabled: false,
 	execute(client, message, args) {
 		const userID = args[0];
-		const user = client.users.resolve(userID);
-		if(!user) return message.channel.send("Couldn't find user to unban!\nPlease try again, ${message.author}!");
+		const user = client.users.resolveID(userID);
+		if(!user) return message.channel.send(`Couldn't find user to unban!\nPlease try again, ${message.author}!`);
 		const reason = args.slice(1).join(" ");
 
 		message.guild.members.unban(user)
