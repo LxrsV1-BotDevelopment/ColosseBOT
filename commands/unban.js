547761c7635b60b63cd9ff791c06379929c13875
Edgars Eduards Hoveckis <53938497+EEHoveckis@users.noreply.github.com>
2020-05-30 01:38:57 +0300

    Testing .fetch() instead of .get()

--- commands/unban.js
+++ commands/unban.js
@@ -12,7 +12,7 @@ module.exports = {
 	disabled: false,
 	execute(client, message, args) {
 		const userID = args[0];
-		const user = client.users.get(userID);
+		const user = client.users.fetch(userID);
 		if(!user) return message.channel.send("Couldn't find user to unban!\nPlease try again, ${message.author}!");
 		const reason = args.slice(1).join(" ");
 
@@ -20,5 +20,3 @@ module.exports = {
 			.then(user => message.channel.send(`Unbanned ${user.username}!`));
 	},
 };
-
-//Template for command.
