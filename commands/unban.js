9723bed4fec1e571d5a1cc5647ed2bd547baca03
Edgars Eduards Hoveckis <53938497+EEHoveckis@users.noreply.github.com>
2020-05-30 01:32:45 +0300

    Created unban command

---
+++ commands/unban.js
@@ -0,0 +1,24 @@
+module.exports = {
+	name: 'unban',
+	description: 'Unbans specified user for specified reason.',
+	usage: '//unban <@user> <Reason>',
+	args: true,
+	argsCount: 2,
+	guildOnly: true,
+	directOnly: false,
+	roleCheck: true,
+	allowedRoles: ["SENIOR STAFF", "MANAGERS", "OWNERS"],
+	cooldown: 3,
+	disabled: false,
+	execute(client, message, args) {
+		const userID = args[0];
+		const user = client.users.get(userID);
+		if(!user) return message.channel.send("Couldn't find user to unban!\nPlease try again, ${message.author}!");
+		const reason = args.slice(1).join(" ");
+
+		message.guild.members.unban(user)
+			.then(user => message.channel.send(`Unbanned ${user.username}!`));
+	},
+};
+
+//Template for command.
