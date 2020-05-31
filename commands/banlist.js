8746c64a509a9cf097a4556354642a94a0a44ab8
Edgars Eduards Hoveckis <53938497+EEHoveckis@users.noreply.github.com>
2020-05-31 18:19:59 +0300

    testing banlist

---
+++ commands/banlist.js
@@ -0,0 +1,17 @@
+module.exports = {
+	name: 'banlist',
+	description: 'Get list of banned users.',
+	usage: '//banlist',
+	args: false,
+	argsCount: 0,
+	guildOnly: true,
+	directOnly: false,
+	cooldown: 3,
+	disabled: false,
+	execute(client, message, args) {
+		const bannedusers = message.guild.fetchBans();
+		if (!bannedusers) return message.channel.send(`There are no banned users in the guild.`);
+	},
+};
+
+//Template for command.
