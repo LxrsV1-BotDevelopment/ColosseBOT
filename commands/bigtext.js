ea35f5990b6a0f0514326730dc5de54dd9e3ae86
Edgars Eduards Hoveckis <53938497+EEHoveckis@users.noreply.github.com>
2020-06-04 21:05:11 +0300

    Created bigtext and smalltext commands

---
+++ commands/bigtext.js
@@ -0,0 +1,17 @@
+module.exports = {
+	name: 'bigtext',
+	description: 'Convert text to uppercase.',
+	usage: '//bigtext <text>',
+	args: true,
+	argsCount: 1,
+	guildOnly: false,
+	directOnly: false,
+	cooldown: 3,
+	disabled: false,
+	execute(client, message, args) {
+		const newText = args.join(" ").toUpperCase();
+		message.channel.send(newText);
+	},
+};
+
+//Template for command.
