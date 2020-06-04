45faebee56db83250323738342450af2f1b84a2f
Edgars Eduards Hoveckis <53938497+EEHoveckis@users.noreply.github.com>
2020-06-04 19:36:07 +0300

    Still trying to fix error

--- commands/banlist.js
+++ commands/banlist.js
@@ -12,19 +12,10 @@ module.exports = {
 	cooldown: 3,
 	disabled: false,
 	execute(client, message, args) {
-		message.guild.fetchBans().then(banned => {
-			if(banned.size < 1) {
-				const noBansEmbed = new Discord.MessageEmbed()
-				.setColor(colorBlack)
-				.setTitle("There are no banned users in the guild.")
-				return message.channel.send({embed: noBansEmbed});
-			} else {
-				message.channel.send("=====Banned Users List=====");
-				message.channel.send(`${banned.size} users are banned:`);
-				banned.forEach(user => user.tag) {
-					message.channel.send(`${user.tag} - ${user.id}`);
-				};
-			}
-		});
+		message.guild.fetchBans().then(bans => {
+          bans.forEach(user => {
+            message.channel.send(user.tag + " " + user.id);
+          })
+      })
 	},
 };
