a3df88cad50c8024851b026d44f793283f0651b0
Edgars Eduards Hoveckis <53938497+EEHoveckis@users.noreply.github.com>
2020-06-25 00:01:28 +0300

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -14,7 +14,6 @@ module.exports = {
 	cooldown: 3,
 	disabled: false,
 	execute(client, message, args) {
-		var a = new Boolean(true);
 		const amount = args[0];
 		if (amount < 2 || amount > 100) return message.channel.send("Min Messages: 2 / Max Messages: 100!");
 		const reason = args.slice(1).join(" ");
@@ -22,11 +21,9 @@ module.exports = {
 		message.channel.bulkDelete(amount).catch(error => {
 			console.log(error.stack);
 			message.channel.send("There was an error trying to execute that command!\nMost probably some of the messages are more than 14 days old.");
-			a = false;
-		});
-		if (a == false){
 			return;
-		} else if (a == true){
+			return;
+		});
 			const purgeEmbed = new Discord.MessageEmbed()
 			.setTitle("ColosseBOT Mod-Logs")
 			.setDescription("Channel purge report.")
@@ -38,6 +35,5 @@ module.exports = {
 			.setFooter("ColosseBOT", botThumbnail);
 
 			client.guilds.resolve(guildID).channels.resolve(modLogsChannel).send({embed: purgeEmbed});
-		}
 	},
 };
