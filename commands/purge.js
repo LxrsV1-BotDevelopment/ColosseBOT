f5c62ae119c8f49b9705ef598285671cb080a67c
Edgars Eduards Hoveckis <53938497+EEHoveckis@users.noreply.github.com>
2020-06-26 13:08:36 +0300

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -22,6 +22,7 @@ module.exports = {
 
 		message.channel.bulkDelete(amount).catch(error => {
 			if(error) {
+				console.log(error.stack);
 				return message.channel.send("There was an error trying to execute that command!\nMost probably some of the messages are more than 14 days old.");
 			}
 		});
