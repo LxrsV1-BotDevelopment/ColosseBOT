71f5ca63ea9b956aa66050936f3bd7699aa6a951
Edgars Eduards Hoveckis <53938497+EEHoveckis@users.noreply.github.com>
2020-07-11 11:29:44 +0300

    Created dog command

---
+++ commands/dog.js
@@ -0,0 +1,24 @@
+const discord = require("discord.js");
+const { get } = require("snekfetch");
+
+module.exports = {
+	name: 'dog',
+	description: 'Sends a random dog image.',
+	usage: '//dog',
+	args: false,
+	argsCount: 0,
+	guildOnly: false,
+	directOnly: false,
+	roleCheck: false,
+	cooldown: 3,
+	disabled: false,
+	execute(client, message, args) {
+    try {
+			get('https://random.dog/woof.json').then(result => {
+				return message.channel.send({files: [{attachment: result.body.file, name: `dog.${result.body.file.split('.')[2]}`}]});
+			});
+		} catch(err) {
+			return console.log(err.stack);
+		  }  
+    }
+  }
