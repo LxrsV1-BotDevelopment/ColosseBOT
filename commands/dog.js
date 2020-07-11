6548706fc3ae8fd256f38f2356f06dd1677588ce
Edgars Eduards Hoveckis <53938497+EEHoveckis@users.noreply.github.com>
2020-07-11 11:34:37 +0300

    Update dog.js

--- commands/dog.js
+++ commands/dog.js
@@ -14,8 +14,8 @@ module.exports = {
 	disabled: false,
 	execute(client, message, args) {
     try {
-			get('https://random.dog/woof.json').then(result => {
-				return message.channel.send({files: [{attachment: result.body.file, name: `dog.${result.body.file.split('.')[2]}`}]});
+			get('https://random.dog/woof').then(result => {
+				return message.channel.send({files: [{attachment: result.body.file, name: `dog.${result.body.file.split('.')[1]}`}]});
 			});
 		} catch(err) {
 			return console.log(err.stack);
