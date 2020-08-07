8d9dee9e43421f251e476341afa7ebce3bac0ece
Edgars Eduards Hoveckis <53938497+EEHoveckis@users.noreply.github.com>
2020-08-07 13:03:14 +0300

    Added possibility to add aliases to commands

--- bot.js
+++ bot.js
@@ -21,8 +21,8 @@ client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).split(/ +/);
     const commandName = args.shift().toLowerCase();
-    if (!client.commands.has(commandName)) return;
-    const command = client.commands.get(commandName);
+    const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aka && cmd.aka.includes(commandName));
+    if (!command) return;
 
     if(command.disabled){
       return message.channel.send('Sorry, but his command is currently disabled!');
