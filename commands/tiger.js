fa76abde2b9a21e75fdc38ca76de5baa3b780564
Edgars Eduards Hoveckis <53938497+EEHoveckis@users.noreply.github.com>
2020-09-20 18:58:04 +0300

    Created multiple animal image commands
    Powered by Unsplash

---
+++ commands/tiger.js
@@ -0,0 +1,42 @@
+const Discord = require("discord.js");
+const { colorWhite } = require("../config.json");
+const fetch = require("node-fetch");
+global.fetch = fetch;
+const Unsplash = require('unsplash-js').default;
+
+
+module.exports = {
+	name: 'tiger',
+	description: 'Sends a random tiger image.',
+	usage: '//tiger',
+	args: false,
+	argsCount: 0,
+	guildOnly: false,
+	directOnly: false,
+	roleCheck: false,
+	cooldown: 3,
+	disabled: false,
+	execute(client, message, args) {
+
+		const unsplash = new Unsplash({
+			accessKey: "NZt1CoteZLqpqhh1Va0bUEWvnCpm7AMCL_sDguu6sCE",
+			secret: "jmbiGER9KonlAni4NSS0eV8fBnaxkjNbfGMyfTL6kVY"
+	 	});
+				unsplash.photos.getRandomPhoto({ query: "tiger" })
+					.then(result => result.json()).then(body => {
+							if(!body) return message.channel.send("Sorry, I couldn't get the image. Try again later.");
+
+							const catEmbed = new Discord.MessageEmbed()
+							.setDescription(`Photo by [${body.user.name}](${body.user.links.html}) on [Unsplash](https://unsplash.com/?utm_source=ColosseBOT&utm_medium=referral)`)
+							.setColor(colorWhite)
+							.setImage(body.urls.raw)
+
+							unsplash.photos.downloadPhoto(body);
+
+							return message.channel.send({embed: catEmbed});
+					}).catch(error => {
+				console.log(error.stack);
+				return message.channel.send("Sorry, I couldn't get the image. Try again later.");
+			});
+  },
+};
