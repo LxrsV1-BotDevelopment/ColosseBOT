265ee76733abaaa529f1d2d30fdf6ebba94baa67
Edgars Eduards Hoveckis <53938497+EEHoveckis@users.noreply.github.com>
2020-09-24 07:32:09 +0300

    Testing chembalance command

---
+++ commands/chembalance.js
@@ -0,0 +1,29 @@
+const { PythonShell } = require("python-shell");
+
+module.exports = {
+  name: 'chembalance',
+  description: 'Balances a chemical equation.',
+  usage: '//chembalance <Equation>',
+  args: true,
+  argsCount: 1,
+  guildOnly: false,
+  directOnly: false,
+  cooldown: 3,
+  disabled: false,
+  execute(client, message, args) {
+    const options = {
+      mode: text,
+      args: [args]
+    }
+
+
+    PythonShell.run('../modules/python_scripts/chembalance.py', options,
+      function(error, results) {
+        if (error) {
+          throw error;
+        } else {
+          console.log(results);
+      }
+    });
+  },
+};
