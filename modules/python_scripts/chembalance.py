2188e0c0ad325999dabd39d507e9ce3c1fadad66
Edgars Eduards Hoveckis <53938497+EEHoveckis@users.noreply.github.com>
2020-09-24 23:59:35 +0300

    Update chembalance.py

--- modules/python_scripts/chembalance.py
+++ modules/python_scripts/chembalance.py
@@ -1,5 +1,6 @@
 import sys
 from chempy import balance_stoichiometry
+from pprint import pprint
 reac, prod = balance_stoichiometry({'ICl', 'H2O'}, {'Cl', 'IO3', 'I2', 'H'})
 print(reac)
 print(prod)
