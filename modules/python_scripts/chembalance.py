7da87c0522cfcd1fb9c8cd8393e52de5b48dbec3
Edgars Eduards Hoveckis <53938497+EEHoveckis@users.noreply.github.com>
2020-09-25 00:17:08 +0300

    Still testing

--- modules/python_scripts/chembalance.py
+++ modules/python_scripts/chembalance.py
@@ -2,6 +2,6 @@ import sys
 from chempy import balance_stoichiometry
 from pprint import pprint
 reac, prod = balance_stoichiometry({'C7H5(NO2)3', 'NH4NO3'}, {'CO', 'H2O', 'N2'})
-pprint(reac)
-pprint(prod)
+pprint(reac.OrderedDict)
+pprint(prod.OrderedDict)
 
