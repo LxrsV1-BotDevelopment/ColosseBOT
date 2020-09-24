52d0726144a0ac3acd16fe9cad64a0ccfd4aaf8f
Edgars Eduards Hoveckis <53938497+EEHoveckis@users.noreply.github.com>
2020-09-24 21:08:14 +0300

    Testing 2

--- modules/python_scripts/chembalance.py
+++ modules/python_scripts/chembalance.py
@@ -2,4 +2,5 @@ from chempy import Equilibrium
 from sympy import symbols
 K1, K2, Kw = symbols('K1 K2 Kw')
 e1 = Equilibrium({'Ba': 1, 'H2SO4': 1}, {'BaSO4': 1, 'H2': 1}, K1)
-print(e1)
+coeff = Equilibrium.eliminate([e1], 'e-')
+print(coeff)
