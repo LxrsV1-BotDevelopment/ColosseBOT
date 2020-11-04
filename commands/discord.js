30f1c07f967d99149877011d939f22cbe5536c9b
Edgars Eduards Hoveckis <53938497+EEHoveckis@users.noreply.github.com>
2020-11-04 03:31:13 +0200

    Polished discord command and created botinfo command

--- commands/discord.js
+++ commands/discord.js
@@ -1,29 +1,20 @@
 const Discord = require("discord.js");
-const { colorWhite, botThumbnail, discordThumbnail } = require("../config.json");
+const { colorDiscord, discordThumbnail } = require("../config.json");
 
 module.exports = {
 	name: 'discord',
-	aka: "discordlinks",
 	description: 'Sends Embed with Discord Links',
 	usage: '//discord',
-	args: false,
-	argsCount: 0,
-	guildOnly: false,
-	directOnly: false,
-	cooldown: 3,
-	disabled: false,
 	execute(client, message, args) {
 		const discordEmbed = new Discord.MessageEmbed()
-		.setTitle("Discord Links")
-		.setColor(colorWhite)
-		.setThumbnail(discordThumbnail)
-		.setDescription("Links to Discord Policies.")
-		.addField("Discord Terms of Service", "[discord.com/terms](https://discord.com/terms)")
-		.addField("Discord Privacy Policy", "[discord.com/privacy](https://discord.com/privacy)")
-		.addField("Discord Community Guidelines", "[discord.com/guidelines](https://discord.com/guidelines)")
-		.addField("Open Source Libraries Used To Make Discord", "[discord.com/acknowledgements](https://discord.com/acknowledgements)")
-		.addField("Licenses for Libraries Discord Uses", "[discord.com/licenses](https://discord.com/licenses)")
-		.setFooter("ColosseBOT is not affiliated or associated in any way with Discord.", botThumbnail)
+		.setTitle("⋙ ColosseBOT || Discord Links ⋘")
+		.setURL("https://colossebot.app")
+		.setColor(colorDiscord)
+		.addField("Discord Help Center:", "[support.discord.com](https://support.discord.com)")
+		.addField("Discord Safety Center:", "[discord.com/safety](https://discord.com/safety)")
+		.addField("Discord Terms of Service:", "[discord.com/terms](https://discord.com/terms)")
+		.addField("Discord Privacy Policy:", "[discord.com/privacy](https://discord.com/privacy)")
+		.addField("Discord Community Guidelines:", "[discord.com/guidelines](https://discord.com/guidelines)")
 
 		message.channel.send({embed: discordEmbed});
 	},
